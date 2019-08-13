d35abc6c23574ec0afd1aa2b4c90c829700c21b2
Robert Johnson <rjohnson2@oreillyauto.com>
2019-08-13 14:48:50 -0500

    connect to mysql on my machine

--- src/models/company.model.ts
+++ src/models/company.model.ts
@@ -1,9 +1,10 @@
 import { Table, Column, Model, HasMany } from 'sequelize-typescript';
-import { User } from './user.model'
-@Table({timestamps: true})
+import { User } from './user.model';
+
+@Table({ timestamps: true })
 export class Company extends Model<Company> {
 
-  @Column({primaryKey: true, autoIncrement: true})
+  @Column({ primaryKey: true, autoIncrement: true })
   id: number;
 
   @Column
